1d931266d13307b78b5cbca82abf12474133af92
Samuel A. Uganiza <sau23@users.noreply.github.com>
2021-03-27 12:57:49 -0400

    create JUnit tests
    -added JUnit 5 to build path

---
+++ java/PatchworkNovels/src/com/PatchworkNovels/test/CommentServiceTest.java
@@ -0,0 +1,64 @@
+package com.PatchworkNovels.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CommentServiceTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAddComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReadComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEditComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLikeComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDislikeComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllComments() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ java/PatchworkNovels/src/com/PatchworkNovels/test/UserServiceTest.java
@@ -0,0 +1,89 @@
+package com.PatchworkNovels.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEditUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddPublishedStory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeletePublishedStory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddPublishedSnippet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeletePublishedSnippet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddFavoriteStory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteFavoriteStory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidateUser() {
+		fail("Not yet implemented");
+	}
+
+}
