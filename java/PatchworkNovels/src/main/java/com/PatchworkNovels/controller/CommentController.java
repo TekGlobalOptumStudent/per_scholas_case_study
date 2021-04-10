e0db766db3d0e62abc05131014ca7de5b39ae627
Samuel A. Uganiza <sau23@scarletmail.rutgers.edu>
2021-04-10 11:42:38 -0400

    setup new controllers

---
+++ java/PatchworkNovels/src/main/java/com/PatchworkNovels/controller/CommentController.java
@@ -0,0 +1,24 @@
+package com.PatchworkNovels.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.PatchworkNovels.service.CommentService;
+import com.PatchworkNovels.service.SnippetService;
+import com.PatchworkNovels.service.StoryService;
+import com.PatchworkNovels.service.UserService;
+
+public class CommentController {
+
+	@Autowired
+	CommentService commentService;
+
+	@Autowired
+	SnippetService snippetService;
+
+	@Autowired
+	StoryService storyService;
+
+	@Autowired
+	UserService userService;
+	
+}
